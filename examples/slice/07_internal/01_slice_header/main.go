1bbf9b70edb5ebc2ec5461774d6a8b5f486d4ed8
jt <jakezj.tan@gmail.com>
2023-04-30 22:39:25 +0800

    fix sliceheader

--- examples/slice/07_internal/01_slice_header/main.go
+++ examples/slice/07_internal/01_slice_header/main.go
@@ -13,9 +13,10 @@ func main() {
 	// See https://pkg.go.dev/unsafe@latest#Pointer
 	sh := (*reflect.SliceHeader)(unsafe.Pointer(&s))
 	
-	fmt.Printf("sh's Data: %v\n", sh.Data)
+	fmt.Printf("sh's Data: %p\n", unsafe.Pointer(sh.Data))
 	fmt.Printf("sh's Len : %v\n", sh.Len) // 5
 	fmt.Printf("sh's Cap : %v\n", sh.Cap) // 5
 	
-	fmt.Printf("s's array: %p\n", s)
+	fmt.Printf("s        : %p\n", s)
+	fmt.Printf("&s       : %p\n", &s)
 }
